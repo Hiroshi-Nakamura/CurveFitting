be75b27c4c285fa0c8a3ac4c82e9e29e86e4f5a0
Hiroshi Nakamura <43459320+Hiroshi-Nakamura@users.noreply.github.com>
2018-10-22 17:24:22 +0900

    add num_param()

--- CurveFitting.hpp
+++ CurveFitting.hpp
@@ -14,6 +14,8 @@ class CurveFitting {
 public:
     CurveFitting(const std::vector<double>& _x, const std::vector<double>& _y) : x(_x), y(_y) { assert(x.size()==y.size()); }
 
+    virtual size_t num_param()=0;
+
     template<typename T>
     T residual(const std::vector<T> param){
         T rtn=AutomaticDifferentiation::zero<T>();
@@ -46,20 +48,50 @@ class LineFitting : public CurveFitting {
     }
 public:
     LineFitting(const std::vector<double>& _x, const std::vector<double>& _y) : CurveFitting(_x,_y) {}
+    virtual size_t num_param(){ return 2; };
+};
+
+class PolynomialFitting : public CurveFitting {
+    size_t n;
+    virtual AutomaticDifferentiation::FuncPtr<double> model(const std::vector<AutomaticDifferentiation::FuncPtr<double>> param, const double x)
+    {
+        auto rtn=param[0];
+        double mul_x=x;
+        for(size_t i=1; i<=n; i++){
+            rtn = rtn+param[i]*mul_x;
+            mul_x *= x;
+        }
+        return rtn;
+    }
+
+    virtual double model(const std::vector<double> param, const double x)
+    {
+        auto rtn=param[0];
+        double mul_x=x;
+        for(size_t i=1; i<=n; i++){
+            rtn = rtn+param[i]*mul_x;
+            mul_x *= x;
+        }
+        return rtn;
+    }
+public:
+    PolynomialFitting(const std::vector<double>& _x, const std::vector<double>& _y, const size_t _n) : CurveFitting(_x,_y), n(_n) {}
+    virtual size_t num_param(){ return n+1; };
 };
 
 class CatenaryFitting : public CurveFitting {
     virtual AutomaticDifferentiation::FuncPtr<double> model(const std::vector<AutomaticDifferentiation::FuncPtr<double>> param, const double x)
     {
-        return -param[0]*cosh(-(x-param[1])/param[0])+param[2];
+        return param[0]*cosh(-(x-param[1])/param[0])+param[2];
     }
 
     virtual double model(const std::vector<double> param, const double x)
     {
-        return -param[0]*cosh(-(x-param[1])/param[0])+param[2];
+        return param[0]*cosh(-(x-param[1])/param[0])+param[2];
     }
 public:
     CatenaryFitting(const std::vector<double>& _x, const std::vector<double>& _y) : CurveFitting(_x,_y) {}
+    virtual size_t num_param(){ return 3; };
 };
 
 class ExponentialFitting : public CurveFitting {
@@ -74,6 +106,24 @@ class ExponentialFitting : public CurveFitting {
     }
 public:
     ExponentialFitting(const std::vector<double>& _x, const std::vector<double>& _y) : CurveFitting(_x,_y) {}
+    virtual size_t num_param(){ return 4; };
+};
+
+class InvCycloidFitting : public CurveFitting { /// Inv means exchanging x y value.
+    virtual AutomaticDifferentiation::FuncPtr<double> model(const std::vector<AutomaticDifferentiation::FuncPtr<double>> param, const double x)
+    {
+        auto theta=acos(1.0-(x-param[2])/param[0]);
+        return param[0]*(theta-sin(theta))+param[1];
+    }
+
+    virtual double model(const std::vector<double> param, const double x)
+    {
+        auto theta=acos(1.0-(x-param[2])/param[0]);
+        return param[0]*(theta-sin(theta))+param[1];
+    }
+public:
+    InvCycloidFitting(const std::vector<double>& _x, const std::vector<double>& _y) : CurveFitting(_x,_y) {}
+    virtual size_t num_param(){ return 3; };
 };
 
 #endif // CURVEFITTING_HPP_INCLUDED
